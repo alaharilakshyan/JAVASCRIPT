7e74c10b214d3fabfaabc69ced04364c299b2cc9
LakshyanAlahari <alahari.lakshyan@gmail.com>
2025-05-04 00:02:05 +0530

    practices

--- practice JS/first1.js
+++ practice JS/first1.js
@@ -2,12 +2,48 @@
 // console.log("Welcome to the world of Javascript!");
 // console.log("this is a sample only!");
 
-name = "Lakshyn Alahari";
-Registration = "12305842";
-n = null;
-m = undefined;
-isAccpeted = false;
-console.log(m);
-console.log(n);
-console.log(Registration);
-console.log(name);
+// name = "Lakshyn Alahari";
+// Registration = "12305842";
+// n = null;
+// m = undefined;
+// isAccpeted = false;
+// console.log(m);
+// console.log(n);
+// console.log(Registration);
+// console.log(name);
+
+
+// var norm = "This is not a very good practice of mentioning variable in JS";
+// var norm = "This is not ";
+// var norm = "This is not a very good practice ";
+// console.log(norm);
+
+
+// let phone_number = 123456789;
+// const number = 9959620900;
+// console.log(number);
+ 
+// phone_number = 4458112542;
+
+// console.log(phone_number);
+
+
+
+
+
+
+// let name = "kindaichi";
+// console.log(name);
+// let amazed = 8987;
+
+// let y = BigInt("334");
+// let z = Symbol("Arrey yentra Idhi??");
+
+
+
+const details = {
+    full_name : "Lakshyan Alahari",
+    age: 20,
+    isStudent : true,
+    cgpa: 9.0,
+};
