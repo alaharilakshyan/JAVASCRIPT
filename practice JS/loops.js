d8df99438f70257fa452df90bcb48e10606b6930
LakshyanAlahari <alahari.lakshyan@gmail.com>
2025-05-05 12:40:03 +0530

    Rearranged the Docs file to another folder.

---
+++ practice JS/loops.js
@@ -0,0 +1,26 @@
+// for(let count = 1; count <= 10; count++){
+//     console.log("All the best for the exam"); //executed 10 times
+// }
+
+//calculating sum of 1 to 5 using for Loop
+// let sum = 0 ;
+// for(let n = 0; n<= 5; n++){
+//     sum = sum + n;
+// }
+// console.log("Sum = ", sum);
+// console.log("the sum is ended");
+
+
+
+//While Loop
+// let n = 2;
+// while(n <= 30){
+//     console.log("Testing while loop");
+//     n++;
+// }
+
+//dow while loop
+let x = 30;
+do{
+    console.log("Testing do-while loop");
+}while(x>=10);
